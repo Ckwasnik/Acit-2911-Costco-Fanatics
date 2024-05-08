c83578169c888f125b30346d26cba7fb4574c7ea
Markusmmmmm <markusmatsumoto@gmail.com>
2024-05-08 10:24:14 -0700

    added blueprints

--- app.py
+++ app.py
@@ -10,25 +10,26 @@ app.instance_path = Path(".").resolve()
 
 db.init_app(app)
 
-@app.route("/courses")
-def course_json():
-    statement = db.select(Course).order_by(Course.name)
-    records = db.session.execute(statement)
-    course = records.scalars().all()
-    return render_template("customers.html", course=course)
+app.register_blueprint(html_bp, url_prefix="/")
 
-@app.route("/students")
-def student_json():
-    with app_context():
-        students = Student.query.order_by(Student.name).all()
-    return render_template("students.html", student=student)
+# @app.route("/courses")
+# def course_json():
+#     statement = db.select(Course).order_by(Course.name)
+#     records = db.session.execute(statement)
+#     course = records.scalars().all()
+#     return render_template("customers.html", course=course)
 
-@app.route("/programs")
-def program_json():
-    with app_context():
-        programs = Program.query.order_by(Program.name).all()
-    return render_template("programs.html", program=program)
+# @app.route("/students")
+# def student_json():
+#     with app_context():
+#         students = Student.query.order_by(Student.name).all()
+#     return render_template("students.html", student=student)
 
+# @app.route("/programs")
+# def program_json():
+#     with app_context():
+#         programs = Program.query.order_by(Program.name).all()
+#     return render_template("programs.html", program=program)
 
 
 if __name__ == "__main__":
