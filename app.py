110965e9b296e30663b5fdab80c4a4accd183dd8
Ckwasnik <kwasnik@live.com>
2024-05-13 18:50:56 -0700

    cleaning up blueprint

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask import Blueprint, render_template, redirect, url_for
 from models import Program, Student, Course, Registration
 from routes.api_courses import api_courses_bp
+from routes.api_schedule import api_schedule_bp
 
 
 app = Flask(__name__, static_url_path='/static')
@@ -15,19 +16,7 @@ db.init_app(app)
 
 app.register_blueprint(api_courses_bp)
 
-@app.route("/students")
-def render_student():
-    statement = db.select(Student).order_by(Student.name)
-    records = db.session.execute(statement)
-    results = records.scalars().all()
-    return render_template("students.html", student=results)
-
-@app.route("/registration")
-def render_registration():
-    statement = db.select(Registration).order_by(Registration.id)
-    records = db.session.execute(statement)
-    results = records.scalars().all()
-    return render_template("registration.html", registration=results)
+app.register_blueprint(api_schedule_bp)
 
 
 if __name__ == "__main__":
