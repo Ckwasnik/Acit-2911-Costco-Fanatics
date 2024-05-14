347b82bf84349827da148a23e37774d1a242b74e
Ckwasnik <kwasnik@live.com>
2024-05-13 18:38:08 -0700

    fix errors

--- manage.py
+++ manage.py
@@ -77,18 +77,10 @@ def create_registration(amount):
 
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> developer
 if __name__ == "__main__":
     drop_tables()
     create_students()
     create_programs()
     create_courses()
-<<<<<<< HEAD
-    create_registration(1)
-=======
-<<<<<<< HEAD
     create_registration(5)
->>>>>>> developer
+
