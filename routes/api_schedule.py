bcfbbf3f700570b3668d7fafd4d23e16077fa4cd
Ckwasnik <kwasnik@live.com>
2024-05-15 21:32:10 -0700

    schedule page functionality update

--- routes/api_schedule.py
+++ routes/api_schedule.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request, render_template
+from models import Course
 
 from db import db
 
@@ -7,4 +8,7 @@ api_schedule_bp = Blueprint("api_schedule", __name__)
 
 @api_schedule_bp.route("/schedule")
 def render_schedule():
-    pass
+    statement = db.select(Course).order_by(Course.name)
+    records = db.session.execute(statement)
+    results = records.scalars().all()
+    return render_template("schedule.html", courses=results)
