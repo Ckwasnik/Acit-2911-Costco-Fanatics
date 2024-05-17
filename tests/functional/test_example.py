dd33bda060e1257866e6899bd4ef50edf71d207a
nana <ssim25@my.bcit.ca>
2024-05-17 09:19:24 -0700

    commit before checkout

--- tests/functional/test_example.py
+++ tests/functional/test_example.py
@@ -15,12 +15,15 @@ def test_invalid_credits():
 def test_invalid_credit():
     with pytest.raises(AttributeError):
         Course("WebDev", "Tim", "3")
+        
 def test_invalid_name():
     with pytest.raises(AttributeError):
         Course("webdev", 7, 3)
+
 def test_id():
     test_id = Registration(2, 4)
     assert test_id.student_id == 2
+
 def test_invalid_type():
     with pytest.raises(AttributeError):
         Registration("2", 4)
