0e2b7f2eefce64e09330dbaecc396de48a83642a
Collin Fung <cfung33@my.bcit.ca>
2024-05-23 20:21:07 -0700

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,14 @@
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import DeclarativeBase
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+logging.debug(f"SQLAlchemy version: {sqlalchemy.__version__}")
 
 if sqlalchemy.__version__.startswith('2'):
+    from sqlalchemy.orm import DeclarativeBase
+
     class Base(DeclarativeBase):
         pass
 else:
